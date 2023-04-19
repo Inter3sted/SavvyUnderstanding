3ca4dfcb39a8031480fb4d39628ad275a6d1a85f
Kolebe Shanks <110436939+Inter3sted@users.noreply.github.com>
2023-04-19 18:39:24 -0500

    Create extraDOM.js

---
+++ extraDOM.js
@@ -0,0 +1,19 @@
+// How can you select all the input elements on a web page and change their background color to yellow?
+
+// How can you select the third paragraph element on a web page and change its font size to 20 pixels?
+
+// How can you select all the images on a web page and add a class "border" to them?
+
+// How can you select the first element with a class "highlight" and change its text to "New Text"?
+
+// How can you select all the elements with a class "row" and hide them from view?
+
+// How can you select all the links on a web page and add a target="_blank" attribute to each one?
+
+// How can you select the first image on a web page and change its source to a different image?
+  
+// How can you select all the headings (h1 through h6) on a web page and change their font family to "Arial"?
+  
+// How can you select all the paragraph elements within a div with class "content" and change their text color to red?
+  
+// How can you select the last item in an unordered list and change its text to "Last Item"?
