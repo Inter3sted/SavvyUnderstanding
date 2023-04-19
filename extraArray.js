e4fb20752551f6eb30d81f95e2ae887b466a33e7
Kolebe Shanks <110436939+Inter3sted@users.noreply.github.com>
2023-04-19 18:43:53 -0500

    Create extraArray.js

---
+++ extraArray.js
@@ -0,0 +1,9 @@
+// Write a function that takes an array of numbers and returns the sum of all the even numbers in the array.
+
+// Create a program that generates a random password consisting of lowercase letters, uppercase letters, and numbers. The password should be 8 characters long and should contain at least one of each type of character.
+
+// Write a program that takes an array of strings and sorts them in alphabetical order.
+
+// Create a function that takes a string as input and returns the number of vowels in the string.
+
+// Write a program that takes an array of numbers and returns the largest number in the array.
